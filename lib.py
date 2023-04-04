d8afb2d9d1dde43dd17133630efed49dbf98a16b
Zico <zicods7@gmail.com>
2023-04-04 08:54:48 +0100

    Added support for multi-label classification.

--- lib.py
+++ lib.py
@@ -13,7 +13,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from fastai.vision.all import untar_data, URLs, Image, Path
 
-def load_mnist_dataset(input_img_size: int = 28, batch_size: int = 64) -> Tuple[DataLoader, DataLoader]:
+def load_mnist_dataset(input_img_size: int = 28, batch_size: int = 64, use_multi_label = True) -> Tuple[DataLoader, DataLoader]:
     # Get data.
     print("Download dataset (if not downloaded already)...")
     path = untar_data(URLs.MNIST)
@@ -25,18 +25,30 @@ def load_mnist_dataset(input_img_size: int = 28, batch_size: int = 64) -> Tuple[
         img_train_data = torch.stack([img_trans(Image.open(o)).squeeze() for o in Path(os.path.join(path, "training", str(digit))).ls()])
         img_test_data = torch.stack([img_trans(Image.open(o)).squeeze() for o in Path(os.path.join(path, "testing", str(digit))).ls()])
         X.append(img_train_data)
-        y.append([digit] * img_train_data.size(0))
         X_val.append(img_test_data)
-        y_val.append([digit] * img_test_data.size(0))
+        if use_multi_label:
+            y_outs = [0]*10
+            y_outs[digit] = 1
+            y.append([y_outs] * img_train_data.size(0))
+            y_val.append([y_outs] * img_test_data.size(0))
+        else:
+            y.append([digit] * img_train_data.size(0))
+            y_val.append([digit] * img_test_data.size(0))
+    # TODO: Need to add blank image dataset here so that we can classify images without digits.
+
     # Create training set - make sure to flatten the image data into a 1D vector.
     X = torch.cat(X).view(-1, input_img_size * input_img_size)
     # Create labels for the output, i.e. binary classification, with 1 representing a 3 and 0 representing a 7.
-    y = torch.tensor(functools.reduce(lambda a, b: a+b, y)).unsqueeze(1)
+    y = torch.tensor(functools.reduce(lambda a, b: a+b, y))
+    if not use_multi_label:
+        y = y.unsqueeze(1)
     # Put together into a dataset that PyTorch supports.
     data_set = list(zip(X, y))
     # Do the same for the validation set.
     X_val = torch.cat(X_val).view(-1, input_img_size * input_img_size)
-    y_val = torch.tensor(functools.reduce(lambda a, b: a+b, y_val)).unsqueeze(1)
+    y_val = torch.tensor(functools.reduce(lambda a, b: a+b, y_val))
+    if not use_multi_label:
+        y_val = y_val.unsqueeze(1)
     validation_set = list(zip(X_val, y_val))
     # Create DataLoader object so that we can perform mini-batch training.
     training_set = DataLoader(data_set, batch_size=batch_size, shuffle=True)
@@ -45,11 +57,15 @@ def load_mnist_dataset(input_img_size: int = 28, batch_size: int = 64) -> Tuple[
     return training_set, testing_set
 
 # Step 1: Create loss function.
-def mnist_loss(predictions: torch.Tensor, targets: torch.Tensor):
+def cross_entropy_loss(predictions: torch.Tensor, targets: torch.Tensor):
     output = predictions.log_softmax(dim=1)
     return (-output[range(targets.size(0)), targets.squeeze()]).mean()
     # return F.cross_entropy(predictions, targets.squeeze()) # Could use this function.
     # return F.nll_loss(output, targets.squeeze()) # Or use this in conjunction with the `output` above.
+
+def binary_cross_entropy_loss(predictions: torch.Tensor, targets: torch.Tensor):
+    predictions = predictions.sigmoid()
+    return -torch.where(targets==1, predictions, 1-predictions).log().mean()
 
 # Step 2: Create optimiser to perform training loop.
 class BasicOptimiser(torch.optim.Optimizer):
@@ -68,7 +84,10 @@ class BasicOptimiser(torch.optim.Optimizer):
 # Step 3: Calculate gradient i.e. how does the loss change with respect to the weights.
 def calc_grad(xb: torch.Tensor, yb: torch.Tensor, model: torch.nn.Module):
     preds = model(xb)
-    loss = mnist_loss(preds, yb)
+    if yb.shape[1] > 1:
+        loss = binary_cross_entropy_loss(preds, yb)
+    else:
+        loss = cross_entropy_loss(preds, yb)
     loss.backward()
     return loss / xb.size(0)
 
@@ -81,18 +100,29 @@ def train_epoch(model: torch.nn.Module, optimiser: torch.optim.Optimizer, datase
 
     return avg_loss.item() / len(dataset)
 
-def calc_batch_accuracy(mdl: torch.nn.Module, X: torch.Tensor, Y: torch.Tensor) -> Tuple[float, float]:
-    _, max_idx_class = mdl(X).softmax(dim=1).max(dim=1)  # [B, n_classes] -> [B], # get values & indices with the max vals in the dim with scores for each class/label
+def calc_batch_accuracy(model: torch.nn.Module, X: torch.Tensor, Y: torch.Tensor) -> Tuple[float, float]:
+    _, max_idx_class = model(X).softmax(dim=1).max(dim=1)  # [B, n_classes] -> [B], # get values & indices with the max vals in the dim with scores for each class/label
     acc = (max_idx_class == Y.squeeze()).sum().item() / X.size(0)
     batch_loss = 0.0
     with torch.no_grad():
-        batch_loss = mnist_loss(mdl(X), Y).item()
+        batch_loss = cross_entropy_loss(model(X), Y).item()
+    return batch_loss, acc
+
+def calc_multi_batch_accuracy(model: torch.nn.Module, X: torch.Tensor, Y: torch.Tensor, threshold = 0.5):
+    pred = model(X)
+    acc = ((pred.sigmoid() > threshold) == Y.bool()).float().mean()
+    batch_loss = 0.0
+    with torch.no_grad():
+        batch_loss = binary_cross_entropy_loss(pred, Y).item()
     return batch_loss, acc
 
 def validate_epoch(model: torch.nn.Module, dataset: DataLoader) -> Tuple[float, float]:
     loss, acc = [], []
     for xb, yb in dataset:
-        l, a = calc_batch_accuracy(model, xb, yb)
+        if yb.shape[1] > 1:
+            l, a = calc_multi_batch_accuracy(model, xb, yb)
+        else:
+            l, a = calc_batch_accuracy(model, xb, yb)
         loss.append(l)
         acc.append(a)
     validation_acc = np.stack(acc)
@@ -116,8 +146,14 @@ def train_model(model: torch.nn.Module, optimiser: torch.optim.Optimizer, traini
 
     return ret
 
-def classify(model: torch.nn.Module, img_path: Union[str, Path], n_dim: int) -> float:
+def classify(model: torch.nn.Module, img_path: Union[str, Path], n_dim: int, use_multi_label = True, threshold = 0.5) -> torch.Tensor:
     img_data = transforms.ToTensor()(Image.open(img_path))
-    digit = torch.argmax(torch.nn.Softmax(dim=1)(model(img_data.view(-1, n_dim))), dim=1)
+    pred = model(img_data.view(-1, n_dim))
+    if use_multi_label:
+        indices = (pred.sigmoid() > threshold)
+        possible_values = torch.arange(10)
+        digit = possible_values[indices.squeeze()]
+    else:
+        digit = torch.argmax(torch.nn.Softmax(dim=1)(pred), dim=1)
 
-    return digit.item()
+    return digit
--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import torch
 # Obtain the data.
 n_dim = 28 * 28
 n_output = 10
-training_set, testing_set = lib.load_mnist_dataset()
+use_multi_label = True
+training_set, testing_set = lib.load_mnist_dataset(use_multi_label=use_multi_label)
 # Define a model.
 n_features = 32
 model = torch.nn.Sequential(
@@ -23,9 +24,9 @@ learning_rate = 0.1
 # opt = lib.BasicOptimiser(model.parameters(), learning_rate)
 opt = torch.optim.SGD(model.parameters(), learning_rate)
 # Train model.
-results = lib.train_model(model, opt, training_set, testing_set, 50)
-# Predict a random image. /Users/zico/.fastai/data/mnist_png/testing/8
-print(lib.classify(model, "/Users/zico/.fastai/data/mnist_png/testing/8/290.png", n_dim))
+results = lib.train_model(model, opt, training_set, testing_set, 150)
+# Predict a random image.
+print(lib.classify(model, "/Users/zico/.fastai/data/mnist_png/testing/8/290.png", n_dim, use_multi_label=use_multi_label))
 # Post-processing of results.
 plot_results = False
 if plot_results:
